6ddfd40ad5fa185b6c449fceb0871200e4afe924
Thai Tran <105292062+Thaivip00@users.noreply.github.com>
2022-05-31 14:31:24 +0700

    Add files via upload
    add new login

---
+++ swp391/src/java/sample/controllers/LoginController.java
@@ -0,0 +1,108 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package sample.controllers;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import sample.user.UserDAO;
+import sample.user.UserDTO;
+
+/**
+ *
+ * @author THAI
+ */
+@WebServlet(name = "LoginController", urlPatterns = {"/LoginController"})
+public class LoginController extends HttpServlet {
+
+    private static final String ERROR = "login.jsp";
+    private static final String AD = "AD";
+    private static final String ADMIN_PAGE = "admin.jsp";
+    private static final String ST = "ST";
+    private static final String STUDENT_PAGE = "student.jsp";
+    private static final String RE = "RE";
+    private static final String RECRUITER_PAGE = "recruiter.jsp";
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        String url = ERROR;
+        try {
+            String userID = request.getParameter("userID");
+            String password = request.getParameter("password");
+            UserDAO dao = new UserDAO();
+            UserDTO loginUser = dao.checkLogin(userID, password);
+            if (loginUser != null) {
+                String roleID = loginUser.getRoleID();
+                HttpSession session = request.getSession();
+                if (AD.equals(roleID)) {
+                    session.setAttribute("LOGIN_USER", loginUser);
+                    url = ADMIN_PAGE;
+                } else if (ST.equals(roleID)) {
+                    session.setAttribute("LOGIN_USER", loginUser);//1 session va 2 request
+                    url = STUDENT_PAGE;
+                } else if (RE.equals(roleID)) {
+                    session.setAttribute("LOGIN_USER", loginUser);//1 session va 2 request
+                    url = RECRUITER_PAGE;
+                } else {
+                    request.setAttribute("ERROR_MESSAGE", "Your role is not support");//còn param user role action
+                    //tạo Attribute ở Requesst cope
+                }
+
+            } else {
+                request.setAttribute("ERROR_MESSAGE", "Incorrect userID or password");//3c param atribute 
+            }
+        } catch (Exception e) {
+            log("Error at LoginController:" + e.toString());
+        } finally {
+            request.getRequestDispatcher(url).forward(request, response);
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
