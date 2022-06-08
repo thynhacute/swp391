a44caee7842f7eb8007d9a7bd407a9f36ac2ce1a
six <quocsy2511@gmail.com>
2022-06-08 23:33:45 +0700

    n

--- swp391/src/java/sample/user/UserDAO.java
+++ swp391/src/java/sample/user/UserDAO.java
@@ -10,7 +10,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import static jdk.nashorn.internal.objects.NativeJava.type;
 import sample.utils.DBUtils;
 
 /**
